54e2d301169f71d2bafeb100e9cf435a0c6e6419
Cosmiconn <135158826+Cosmiconn@users.noreply.github.com>
2026-07-11 23:00:14 +0200

    Fixed Seed assert error

--- src/core/profiling/tracy_seed.h
+++ src/core/profiling/tracy_seed.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "core/profiling/seed_assert.h"
+
 // ---------------------------------------------------------------------------
 // Tracy integration – SINGLE source of truth for all profiling macros.
 // Include this header everywhere; never redefine SEED_ZONE / SEED_ALLOC etc.
